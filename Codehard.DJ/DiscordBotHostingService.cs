9b46d7593b16dbac4126ca2d602a12518580f448
Wasupol.S <wasupol.s@codehard.co.th>
2023-01-23 22:07:28 +0700

    Add logging

--- Codehard.DJ/DiscordBotHostingService.cs
+++ Codehard.DJ/DiscordBotHostingService.cs
@@ -7,15 +7,18 @@ public class DiscordBotHostingService : IHostedService
 {
     private readonly DjDiscordClient _discordClient;
     private readonly IMusicProvider _provider;
+    private readonly ILogger<DiscordBotHostingService> _logger;
 
     public DiscordBotHostingService(
         DjDiscordClient discordClient,
-        IMusicProvider provider)
+        IMusicProvider provider,
+        ILogger<DiscordBotHostingService> logger)
     {
-        _discordClient = discordClient;
+        this._discordClient = discordClient;
+        this._logger = logger;
         this._provider = provider;
-        this._provider.PlayStartEvent += (sender, args) => { Console.WriteLine($"{args.Music.Title} started"); };
-        this._provider.PlayEndEvent += (sender, args) => { Console.WriteLine($"{args.Music.Title} ended"); };
+        this._provider.PlayStartEvent += (sender, args) => { this._logger.LogInformation($"{args.Music.Title} started"); };
+        this._provider.PlayEndEvent += (sender, args) => { this._logger.LogInformation($"{args.Music.Title} ended"); };
     }
 
     public async Task StartAsync(CancellationToken cancellationToken)
